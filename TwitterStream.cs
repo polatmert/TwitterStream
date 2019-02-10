ca32bd207af820f0bcb82928470d25a3ce3d4324
polatmert <husnumertpolatt@gmail.com>
2019-02-10 19:39:39 +0300

    Initial commit

---
+++ TwitterStream.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+//form application resource : https://www.youtube.com/watch?v=3KnDstiN6qc
+namespace TwitterStream
+{
+    class TwitterStream
+    {
+        static void Main(string[] args)
+        {
+            //Authentication
+            Auth.SetUserCredentials(ConfigurationManager.AppSettings["ConsumerKey"], ConfigurationManager.AppSettings["ConsumerSecret"], ConfigurationManager.AppSettings["AccessToken"],
+                ConfigurationManager.AppSettings["AccessTokenSecret"]);
+
+            //Create the Stream
+            var stream = Stream.CreateFilteredStream();
+
+            //Keywords to Track 
+            stream.AddTrack("disney");
+
+            //Limit to English
+            stream.AddTweetLanguageFilter(LanguageFilter.English);
+
+            //Message so you know its running
+            Console.Write("I am listening to Twitter");
+
+            stream.MatchingTweetReceived += (sender, arguments) =>
+            {
+                Console.WriteLine(arguments.Tweet.Text);
+            };
+
+            stream.StartStreamMatchingAllConditions();
+
+            
+            Console.ReadKey();
+        }
+    }
+}
